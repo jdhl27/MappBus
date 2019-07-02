4762dd15054f394d0fa28c6c57c5b1ed016add90
indep <independientemedellin.27.03@gmail.com>
2019-07-01 22:38:40 -0500

    Actualización 01 de julio del 2019.
    Por: Juan David Hdez

--- registro.php
+++ registro.php
@@ -3,15 +3,11 @@
 //LLAMANDO CONEXION
 require("conectarBD.php");
 
-$nombre = $_POST['inputNombre'];
-$direccion = $_POST['inputDireccion'];
-$latitud = $_POST['inputLat'];
-$longitud = $_POST['inputLng'];
-$tipo = $_POST['inputType'];
+
 
 
 //INSERTANDO DATOS A BASE DE DATOS
-if (mysqli_query($conectar,"INSERT INTO ubicacion (nombre,direccion,lat,lng,tipo)
+if (mysqli_query($conectar,"INSERT INTO ubicacionEmpresa (nombreEmpresa,direccionEmpresa,latitud,longitud,tipo)
                               VALUES ('$nombre','$direccion','$latitud','$longitud','$tipo')"))
 {
     echo "BIEN";
